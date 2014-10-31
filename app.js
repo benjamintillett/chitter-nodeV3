fa3fd1c60da567bf988750f13fec54c0749af4b2
benjamin tillett <benjamin.tillett@gmail.com>
2014-10-31 12:30:33 +0000

    add socket io

--- app.js
+++ app.js
@@ -9,18 +9,24 @@ db.on('error', function () {
   throw new Error('unable to connect to database at ' + config.db);
 });
 
+
 var models = glob.sync(config.root + '/app/models/*.js');
 models.forEach(function (model) {
   require(model);
 });
 
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 require('./config/express')(app, config);
 
+io.on("connection",function(socket){
+	console.log("a user connected");
+});
 
-module.exports = app;
+module.exports = http;
 if (!module.parent) {
   console.log('Server running on port ' + config.port)
-  app.listen(config.port)
+  http.listen(config.port)
 }
