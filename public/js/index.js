6773d5030f127768f4cb619db210eb04dd32da8b
benjamin tillett <benjamin.tillett@gmail.com>
2014-11-04 08:06:06 +0000

    remove sockets, revert to orginal message creation through posting

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,4 @@
-$(document).ready(function(){
-  $('.message-form').on('submit', function(e) {
-    e.preventDefault();
-    socket.emit('message', $('#message-content').val());
-    $('#message-content').val("");
-    return false;
-  })
-});	
+
 
 
   
